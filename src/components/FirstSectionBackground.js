3df28c32a7fcb10f3c161045aeeecd02a23aaed3
averinpassmore <averinpassmore@gmail.com>
2022-05-13 14:22:22 -0700

    wireframes for first section completed

--- src/components/FirstSectionBackground.js
+++ src/components/FirstSectionBackground.js
@@ -5,6 +5,8 @@ const FirstSectionBackground = () => {
   return (
     <div className="First-section">
       <img src="https://ychef.files.bbci.co.uk/976x549/p087r1np.jpg" className="Background"></img>
+      <p className="First-section-text">Best Locksmith in Southern Nevada!</p>
+      <p className="First-section-text-smaller">~Over 40 years of experience~</p>
       <button className="First-section-button">Get a quote!</button>
     </div>
   )
