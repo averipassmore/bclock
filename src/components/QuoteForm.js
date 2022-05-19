21cba6b7c984597bd05b075730c8bb805c812e35
averinpassmore <averinpassmore@gmail.com>
2022-05-19 15:34:13 -0700

    netlify form test

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,11 +1,34 @@
 import React from "react";
 
 const QuoteForm = () => {
+  // function encode(data) {
+  //   return Object.keys(data)
+  //     .map(
+  //       (key) =>
+  //         encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+  //     )
+  //     .join("&");
+  // }
+  
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   fetch("/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: encode({
+  //       "form-name": event.target.getAttribute("name"),
+  //       // ...name,
+  //     }),
+  //   })
+  //     // .then(() => navigate("/thank-you/"))
+  //     .catch((error) => alert(error));
+  // };
   return (
     <div>
-      <div className="Wrapper-div"></div>
+      {/* <div className="Wrapper-div"></div>
       <div className="Quote-form-wrapper">
-        <form name="contact" netlify>
+        <form data-netlify="true" name="contactInfo" method="post" onSubmit={handleSubmit}>
+          <input type={'hidden'} name='quote-form' value={'contactInfo'} />
           <label for='name'>Name</label>
           <input type={'text'} name='name'></input>
 
@@ -20,7 +43,28 @@ const QuoteForm = () => {
 
           <button type="submit">Send</button>
         </form>
-      </div>
+      </div> */}
+      <div className="Wrapper-div"></div>
+      <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <textarea name="message"></textarea>
+      </form>
+      <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
     </div>
   )
 }
