eeafc5b10c870cae00580c281214eae02cee4718
averinpassmore <averinpassmore@gmail.com>
2022-05-18 23:43:07 -0700

    fixed scroll bars for reviews

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -2,8 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Services from "./Services";
 import skelly from "../skellyLogo.png";
+import { scroller } from "react-scroll/modules";
 
 const FirstSection = () => {
+  const scrollToSection = () => {
+    if (window.pageYOffset < 721) {
+      scroller.scrollTo("Services-component", {
+        offset: -100
+      })
+    } else if (721 < window.pageYOffset < 1418) {
+      scroller.scrollTo("Reviews-wrapper", {
+        offset: -100
+      })
+    }
+    console.log(window.pageYOffset)
+  }
+
   return (
     <div id="First-section-component">
       <div className="Wrapper-div"></div>
@@ -17,7 +31,7 @@ const FirstSection = () => {
         <h1 className="First-section-text-2">"For all your security needs!"</h1>
       </div>
       <div className="First-section-footer">
-        <a href="#Services-component" className="Navigate-down-button"></a>
+        <button className="Navigate-down-button" onClick={scrollToSection}></button>
       </div>
       <Services />
     </div>
