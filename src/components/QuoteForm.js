9bb109da4abe329dac1fae06be8e9cc20ff1900b
averinpassmore <averinpassmore@gmail.com>
2022-05-19 17:40:02 -0700

    hitting submit on quote form send email to business owner and auto replys to customer

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,72 +1,41 @@
-import React from "react";
+import React, {useRef} from "react";
+// import emailjs from 'emailjs-com';
+// import { Link } from "react-router-dom";
 
 const QuoteForm = () => {
-  // function encode(data) {
-  //   return Object.keys(data)
-  //     .map(
-  //       (key) =>
-  //         encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-  //     )
-  //     .join("&");
-  // }
-  
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: encode({
-  //       "form-name": event.target.getAttribute("name"),
-  //       // ...name,
-  //     }),
-  //   })
-  //     // .then(() => navigate("/thank-you/"))
-  //     .catch((error) => alert(error));
-  // };
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    // emailjs.sendForm('service_vxo5ko4', 'template_bnbzl4f', form.current, 'Um-6jVm3sb72fybJ3')
+    //   .then((result) => {
+    //       console.log(result.text);
+    //       alert('Success! Redirecting to home...');
+    //   }, (error) => {
+    //       console.log(error.text);
+    //       alert("Failed...", error)
+    //   });
+  };
+
   return (
     <div>
-      {/* <div className="Wrapper-div"></div>
-      <div className="Quote-form-wrapper">
-        <form data-netlify="true" name="contactInfo" method="post" onSubmit={handleSubmit}>
-          <input type={'hidden'} name='quote-form' value={'contactInfo'} />
-          <label for='name'>Name</label>
-          <input type={'text'} name='name'></input>
-
-          <label for='email'>Email</label>
-          <input type={'email'} name='email'></input>
-
-          <label for='phone'>Phone</label>
-          <input type={'tel'} name='phone' pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"></input>
-
-          <label for='summary'>How can we help?</label>
-          <input type={'text'} name='summary'></input>
-
-          <button type="submit">Send</button>
-        </form>
-      </div> */}
       <div className="Wrapper-div"></div>
-      <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <textarea name="message"></textarea>
-      </form>
-      <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+      <div className="Quote-form-background">
+        <form ref={form} onSubmit={sendEmail} className="Quote-form">
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Phone</label>
+          <input type="tel" name="user_phone" pattern="[0-9]{10}"/>
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
         </form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default QuoteForm;
