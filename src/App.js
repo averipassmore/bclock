3b2fb48c7e9f0c2cd2b42eb566cbe59640f3e968
averinpassmore <averinpassmore@gmail.com>
2022-05-27 01:36:55 -0700

    fixed window resizing causing button transitions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import MainRoutes from './components/MainRoutes';
 
 function App() {
+  let resizeTimer;
+  window.addEventListener('resize', () => {
+    console.log('resizing');
+    document.body.classList.add('Resize-transition-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      document.body.classList.remove('Resize-transition-stopper');
+    }, 400)
+  })
   return (
     <div>
       <MainRoutes />
